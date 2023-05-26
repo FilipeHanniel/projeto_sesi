e52509a4eee2182ab16c551664d5ac00d42adef1
Filipe Hanniel <filipehanniel@gmail.com>
2023-05-25 21:51:06 -0300

    Aula 25/05/2023

---
+++ src/paralelo/Bank.java
@@ -0,0 +1,22 @@
+package paralelo;
+
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class Bank {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        Account account = new Account(10, "Filipe")
+
+        System.out.println(account.getCode());
+        System.out.println(Account.getHolder());
+        sout
+        sc.close();
+
+    }
+    
+}
